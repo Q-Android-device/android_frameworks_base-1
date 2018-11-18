b38aaa2c78ce7121d3a91ec7b734dfea720b5f2a
Lucchetto <lucchetto.tie@live.com>
2018-11-18 19:02:33 +0100

    Always block sensors for all packages
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14132,12 +14132,6 @@ public final class Settings {
         public static final String SHOW_CPU_OVERLAY = "show_cpu_overlay";
 
         /**
-         * Sensor block per-package
-         * @hide
-         */
-        public static final String SENSOR_BLOCK = "sensor_block";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
